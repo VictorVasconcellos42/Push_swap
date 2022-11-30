13d9ab995904a7660030a1bc18eedf97c2f26a4e
vde-vasc <vde-vasc@student.42.rio>
2022-11-30 18:45:13 -0300

    SOURCE dir created

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,23 +6,25 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:04:41 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/11/22 09:58:53 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/11/30 16:09:43 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "../includes/push_swap.h"
 
 int main(int argc, char **argv)
 
 {
 	int i;
 
+
 	i = 0;
 	if (argc > 3)
 	{
-		while (argv[i])
-			printf("%s\n", argv[i++]);
-	
+		while (i < argc)
+		{
+			ft_printf("%s\n", argv[i++]); 
+		}
 	}
 	return (0);
 }
