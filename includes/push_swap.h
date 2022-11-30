59ba2e8db5193dd906ccc427445e5b3322695c9e
vde-vasc <vde-vasc@student.42.rio>
2022-11-30 18:44:45 -0300

    Header created

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,15 +6,24 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 10:25:04 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/11/22 10:28:17 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/11/30 15:59:59 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "utils/libft/libft.h"
+# include "../utils/libft/libft.h"
+# include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
+
+typedef struct	s_stack
+
+{
+	void *content;
+
+	struct s_stack *prev;
+	struct s_stack *next;
+} t_stack ;
 
 # endif
