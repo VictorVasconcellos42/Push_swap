a1e186690bcea561330589779a6cbfb88e526bb6
Victor Vasconcellos <54756218+VictorVasconcellos42@users.noreply.github.com>
2022-12-06 22:43:04 +0000

    create_array function created

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,25 +6,37 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:04:41 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/11/30 16:09:43 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/12/06 22:39:19 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
+int	*create_array(int argc, char **argv)
+
+{
+	int	*array;
+	int	i;
+	int j;
+
+	i = 0;
+	j = 1;
+	array = malloc((argc - 1) * sizeof(int));
+	if (!array)
+		return (NULL);
+	while (--argc > 0)
+		array[i++] = ft_atoi(argv[j++]);
+	return (array);
+}
+
 int main(int argc, char **argv)
 
 {
-	int i;
-
-
-	i = 0;
+	int *array;
+	
 	if (argc > 3)
 	{
-		while (i < argc)
-		{
-			ft_printf("%s\n", argv[i++]); 
-		}
+		array = create_array(argc, argv);
 	}
 	return (0);
 }
