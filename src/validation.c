5a7583726535388ab19290277cb6099dd8a1fd3b
Victor Vasconcellos <54756218+VictorVasconcellos42@users.noreply.github.com>
2022-12-07 03:25:59 +0000

    Only number function created

--- src/validation.c
+++ src/validation.c
@@ -6,13 +6,30 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 13:53:21 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/12/06 21:21:34 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/12/07 03:06:59 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int checker(char c)
+int only_number(char **array)
 
 {
+    int i;
+    int j;
+    
+    i = 0;
+    while (array[++i])
+    {
+        j = -1;
+        while (array[i][++j])
+        {
+            if (!ft_isdigit(array[i][j]))
+            {
+               ft_putendl_fd("Error", 2);
+               exit(1);
+            }
+        }
+    }
+    return (0);
 }
