f4d57a341f76d4f01301f0d7d3fb3c48e403958a
Victor Vasconcellos <54756218+VictorVasconcellos42@users.noreply.github.com>
2022-12-07 22:24:30 +0000

    add news prototypes and new macro in my header

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 10:25:04 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/12/07 12:52:02 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/12/07 21:26:25 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include "../utils/libft/libft.h"
 # include <unistd.h>
 # include <stdlib.h>
+
+# define RUN 64
 
 typedef struct s_stack
 {
@@ -27,4 +29,9 @@ int		only_number(char **argv);
 void	error_msg(int fd);
 int		has_duplicate(int len, int *array);
 int		ft_isorder(int len, int *array);
+int		checker_array(int len, int *array);
+int		min(int a, int b);
+void	insertion(int *array, int left, int right);
+void	merge(int *array, int l, int m, int r);
+void	timsort(int len, int *array);
 #endif
