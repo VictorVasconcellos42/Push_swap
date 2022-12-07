9cc7c0720225ce17617d34c1a2d50a5cb7793d9b
Victor Vasconcellos <54756218+VictorVasconcellos42@users.noreply.github.com>
2022-12-07 12:31:12 +0000

    New function to valid if has duplicates int

--- src/validation.c
+++ src/validation.c
@@ -6,11 +6,32 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 13:53:21 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/12/07 12:16:13 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/12/07 12:30:39 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+int	has_duplicate(int len, char **array)
+
+{
+	int i;
+	int	j;
+	
+	i = 0;
+	while (i <= len)
+	{
+		j = i + 1;
+		while (j <= len)
+		{
+			if (array[i] == array[j])
+				error_msg(2);
+			j++
+		}
+		i++;
+	}
+	return (0);
+}
 
 static int	ft_is_signal(char c)
 
