4f08dcc54550709d6ddbe60f3d922a8bb83be453
Victor Vasconcellos <54756218+VictorVasconcellos42@users.noreply.github.com>
2022-12-07 22:24:49 +0000

    New functinn swap add

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,37 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:04:41 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/12/07 12:52:30 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/12/07 21:17:59 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+/* void	bubble_sort(int len, int *array)
+
+{
+	int i;
+	int j;
+	int swap;
+	int step = 0;
+
+	i = 0;
+	while (++i < len)
+	{
+		j = -1;
+		while (++j < len - 1)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
+				step++;
+			}
+		}
+	}
+	ft_printf("step: [%i]\n", step);
+} */
 
 int	*create_array(int argc, char **argv)
 
@@ -37,11 +63,13 @@ int	main(int argc, char **argv)
 
 	if (argc >= 3)
 	{
+		ft_printf("NUMBER ARGUMENTS: [%i]\n", argc - 1);
 		i = 0;
 		only_number(argv);
 		array = create_array(argc, argv);
-		has_duplicate(argc - 1, array);
-		ft_isorder(argc - 1, array);
+		if (checker_array(argc - 1, array) == 1)
+			return (0);
+		timsort(argc - 1, array);
 		while (--argc > 0)
 			ft_printf("%i\n", array[i++]);
 	}
