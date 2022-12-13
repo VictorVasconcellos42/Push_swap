c002dc31dedde0e30d11e458f4b5666b27484068
vde-vasc <vde-vasc@student.42.rio>
2022-12-13 01:50:39 +0000

    Add operations functions in my header

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 10:25:04 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/12/12 21:47:40 by codespace        ###   ########.fr       */
+/*   Updated: 2022/12/13 01:43:17 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ typedef struct s_stack
 {
 	int	*a;
 	int	*b;
-	int	len;
+	int	len_a;
+	int	len_b;
 	
 }	t_stack;
 
@@ -32,10 +33,17 @@ void	error_msg(int fd);
 int		has_duplicate(t_stack *stack);
 int		ft_isorder(t_stack *stack);
 int		checker_array(t_stack *stack);
+void	init_stack_b(t_stack *stack);
 int		min(int a, int b);
-void	insertion(int *array, int left, int right);
-void	merge(int *array, int l, int m, int r);
-void	timsort(int len, int *array);
+void	ss(t_stack *stack);
+void	sb(int *stack_b, int valid);
+void	sa(int *stack_a, int valid);
 void	ra(t_stack	*stack, int valid);
+void    rra(t_stack	*stack, int valid);
+void    rrb(t_stack	*stack, int valid);
+void	pa(t_stack *stack, int valid);
+void    pb(t_stack *stack, int valid);
+
+
 
 #endif
