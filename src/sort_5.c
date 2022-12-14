ddeb70556f5cabc7d1998fba7b2d0ebaa4671199
vde-vasc <vde-vasc@student.42.rio>
2022-12-14 14:11:59 +0000

    sort five algorithm created

--- src/sort_5.c
+++ src/sort_5.c
@@ -6,20 +6,51 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 11:49:12 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/12/14 12:43:08 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/12/14 14:03:22 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	find_lowers(t_stack *stack)
+int	find_lowers(t_stack *stack)
 
 {
-	return ;
+	int min;
+	int	i;
+
+	min = INT_MAX;
+	i = 0;
+	while (i < stack->len_a)
+	{
+		if (stack->a[i] < min)
+		    min = stack->a[i];
+        i++;
+	}
+	return (min);
 }
 
 void	sort_five(t_stack	*stack)
 
 {
-	return ;
+	int	small;
+	int i;
+	
+	while (stack->len_a > 3)
+	{
+		small = find_lowers(stack);
+		i = 0;
+		if (stack->a[i] != small)
+		{
+			while (stack->a[i] != small)
+			{
+				if (stack->a[i] == small)
+					break ;
+				rra(stack, 0);
+			}
+		}
+		pb(stack, 0);
+	}
+	sort_three(stack);
+	pa(stack, 0);
+	pa(stack, 0);
 }
