a115d09bf80e92e4c8c4d74e82bf94baf2ae6ea1
vde-vasc <vde-vasc@student.42.rio>
2022-12-14 10:19:22 +0000

    add case 3,4 and 5 in sort function

--- src/sort_3.c
+++ src/sort_3.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 09:56:13 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/12/14 10:03:28 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/12/14 10:15:57 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,19 @@ void    sort_three(t_stack *stack)
 
 {
     if (stack->a[0] > stack->a[1] && stack->a[1] < stack->a[2] && stack->a[0] < stack->a[2])
-        sa(stack->a, 1);
+    	sa(stack->a, 0);
     else if (stack->a[0] < stack->a[1] && stack->a[1] > stack->a[2] && stack->a[0] > stack->a[2])
-        rra(stack, 1);
+    	rra(stack, 0);
+    else if (stack->a[0] > stack->a[1] && stack->a[0] > stack->a[2] && stack->a[1] < stack->a[2])
+		ra(stack, 0);
+    else if (stack->a[0] > stack->a[1] && stack->a[0] > stack->a[2] && stack->a[1] > stack->a[2])
+    {
+		sa(stack->a, 0);
+		rra(stack, 0);
+    }
+    else if (stack->a[0] < stack->a[1] && stack->a[1] > stack->a[2] && stack->a[2] > stack->a[0])
+    {
+		rra(stack, 0);
+		sa(stack->a, 0);
+    }
 }
