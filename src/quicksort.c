0719d7668fb3187d6196db3cd3d63f55571e8c91
vde-vasc <vde-vasc@student.42.rio>
2022-12-20 12:36:00 +0000

    quicksort v4: not working

--- src/quicksort.c
+++ src/quicksort.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 19:13:53 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/12/20 05:24:35 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/12/20 12:23:53 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	quick_b(t_stack *stack, int middle)
 {
     while (stack->len_b < middle)
     {
-		if (stack->a[0] <= middle)
+		if (stack->a[0] <= middle && stack->a[0] > stack->index)
 			pb(stack, 0);
 		else
 			ra(stack, 0);
@@ -57,57 +57,51 @@ void	insert_index(t_stack *stack)
 		}
 		i++;
 	}
-	free(stack->c);
 }
 
 void quicksort(t_stack *stack)
 
 {
-	int i;
+	int median;
 
-	i = -1;
-	i++;
+	stack->index = 1;
+	median = search_median(stack->a, stack->len_a);
 	bubble_sort(stack->len_a, stack->c);
-	for (int i = 0; i < stack->len_a; i++)
-	{
-		ft_printf("STACK A: [%i] - [%p] \t|", stack->a[i], &stack->a[i]);
-		ft_printf("STACK C: [%i] - [%p]", stack->c[i], &stack->c[i]);
-		ft_printf("\n");
-	}
 	insert_index(stack);
-	quick_b(stack, stack->len_a / 2);
-	ft_printf("----------------------------------\n");
-	for (int i = 0; i < 15; i++)
-	{
-		ft_printf("STACK A: [%i] - [%p] \t|", stack->a[i], &stack->a[i]);
-		ft_printf("STACK B: [%i] - [%p]", stack->b[i], &stack->b[i]);
-		ft_printf("\n");
-	}
-	ft_printf("----------------------------------\n");
-	if (stack->len_b > 5)
-		quick_a(stack, search_median(stack->b, stack->len_b));
-	bubble_sort(stack->len_b, stack->b);
-	while (stack->len_b > 0)
-	{
-		pa(stack, 0);
-		ra(stack, 0);
-	}
-	while (stack->len_b < 9)
-	{
-		if (stack->a[0] == stack->a[stack->len_a - 1] + 1)
+	quick_b(stack, median);
+	while (!ft_isorder(stack) || stack->len_b != 0)
+    {
+		for (int i = 0; i < stack->len_a; i++)
+		{
+			ft_printf("STACK A: [%i] - [%p] \t|", stack->a[i], &stack->a[i]);
+			ft_printf("STACK C: [%i] - [%p]", stack->c[i], &stack->c[i]);
+			ft_printf("\n");
+		}
+		if (stack->len_b == 0)
+			quick_b(stack, median);
+		ft_printf("----------------------------------\n");
+		for (int i = 0; i < 15; i++)
+		{
+			ft_printf("STACK A: [%i] - [%p] \t|", stack->a[i], &stack->a[i]);
+			ft_printf("STACK B: [%i] - [%p]", stack->b[i], &stack->b[i]);
+			ft_printf("\n");
+		}
+		ft_printf("----------------------------------\n");
+		if (stack->len_b > 5)
+			quick_a(stack, search_median(stack->b, stack->len_b));
+		bubble_sort(stack->len_b, stack->b);
+		while (stack->len_b > 0)
+		{
+			pa(stack, 0);
 			ra(stack, 0);
-		pb(stack, 0);
-	}
-	bubble_sort(stack->len_b, stack->b);
-	while (stack->len_b > 0)
-	{
-		pa(stack, 0);
-		ra(stack, 0);
-	} 
-	for (int i = 0; i < 15; i++)
-	{
-		ft_printf("STACK A: [%i] - [%p] \t|", stack->a[i], &stack->a[i]);
-		ft_printf("STACK B: [%i] - [%p]", stack->b[i], &stack->b[i]);
-		ft_printf("\n");
+		}
+		stack->index = median;
+		median = search_median(stack->a, stack->len_a);
+		for (int i = 0; i < 15; i++)
+		{
+			ft_printf("STACK A: [%i] - [%p] \t|", stack->a[i], &stack->a[i]);
+			ft_printf("STACK B: [%i] - [%p]", stack->b[i], &stack->b[i]);
+			ft_printf("\n");
+		}
 	}
 }
