8388ec66c8338ee880a707c5f157d5ddea98cccd
vde-vasc <vde-vasc@student.42.rio>
2022-12-20 07:04:01 +0000

    maybe radix 100%?

--- src/radix.c
+++ src/radix.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 05:35:26 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/12/20 06:52:30 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/12/20 07:03:32 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,29 @@
 static int	last_number(t_stack *stack, int index)
 
 {
-	return (stack->a[stack->len_a - index]);
+	return (stack->a[stack->len_a - index - 1]);
+}
+
+void	insert_index(t_stack *stack)
+
+{
+	int i;
+	int j;
+	i = 0;
+	while (i < stack->len_a)
+	{
+		j = 0;
+		while (j < stack->len_a)
+		{
+			if (stack->a[i] == stack->c[j])
+			{
+				stack->a[i] = ++j;
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
 void	radix(t_stack *stack)
@@ -28,7 +50,8 @@ void	radix(t_stack *stack)
 	i = 0;
 	j = 0;
     size = stack->len_a;
-	while (ft_isorder(stack) == 0)
+	insert_index(stack);
+	while (ft_isorder(stack) != 0)
 	{
 		j = 0;
 		while (j <= size)
@@ -37,7 +60,7 @@ void	radix(t_stack *stack)
 				ra(stack, 0);
 			else
 				pb(stack, 0);
-			while (stack->len_b >= 0)
+			while (stack->len_b > 0)
 				pa(stack, 0);
 			j++;
 		}
